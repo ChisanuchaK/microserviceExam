170802252d54c35e715298f7eb75722aa0a50cf6
ChisanuchaK <97660202+ChisanuchaK@users.noreply.github.com>
2023-04-26 19:37:09 +0700

    first commit

---
+++ springCould/src/main/java/com/example/springCould/SpringCouldApplication.java
@@ -0,0 +1,13 @@
+package com.example.springCould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCouldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCouldApplication.class, args);
+	}
+
+}
